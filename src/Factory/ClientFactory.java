61cc6c5199e4a99ec14b3a999f5841154a656dd5
Janam <Janam@host.docker.internal>
2023-10-25 11:41:48 +0200

    Clientfactory Done

--- src/Factory/ClientFactory.java
+++ src/Factory/ClientFactory.java
@@ -1,10 +1,40 @@
 package Factory;
 
+import Classes.SigninSignup;
+import Interface.ClientImplementation;
+
 /**
- * 
+ * The `ClientFactory` class is responsible for creating and providing instances
+ * of the `SigninSignup` interface.
+ *
  * @author Janam
  */
 public class ClientFactory {
 
+    // Static variable to store the SigninSignup instance.
+    private static SigninSignup data;
+
+    /**
+     * Get an instance of the SigninSignup interface. If an instance does not
+     * exist, it is created.
+     *
+     * @return An instance of the SigninSignup interface.
+     */
+    public static SigninSignup getSigninSignup() {
+
+        // Check if the data instance is not previously loaded.
+        if (data == null) {
+
+            /**
+             * If not loaded, create a new instance of SigninSignup using the
+             * ClientImplementation class.
+             */
+            data = (SigninSignup) new ClientImplementation();
+
+        }
+
+        // Return the SigninSignup instance.
+        return data;
+    }
 
 }
