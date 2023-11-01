45fbcf18b658fbf83829b7805ee4573d65ccac91
Iñigo <inigo@MacBook-Pro-de-Mikel.local>
2023-11-01 20:03:17 +0100

    Javadoc added

--- View/Logged/LoggedController.java
+++ View/Logged/LoggedController.java
@@ -27,55 +27,69 @@ import javafx.stage.Stage;
 /**
  * FXML Controller class
  *
- * @author 2dam
+ * @author Iñigo
  */
 public class LoggedController extends GenericController{
-
+	/**
+	 * Button to log out.
+	 */
 	@FXML
 	private Button btnLogOut1;
+	/**
+	 * Label to welcome the user.
+	 */
 	@FXML
 	private Label lblWelcome;
 
 	/**
 	 * Initializes the controller class.
+	 * @param root Parent object.
+	 * @param user User object with the user's data.
 	 */
 	public void initStage(Parent root, User user) {
 		try {
 			LOGGER.info("Initializing Logged stage.");
-
+			// Set a title to the window
 			stage.setTitle("Logged");
+			// Set the window icon
 			stage.getIcons().add(new Image("/Resources/logo.png"));
+			// Create and set the scene
 			stage.setScene(new Scene(root));
+			// Set window non-resizable
 			stage.setResizable(false);
+			// Show the window
 			stage.show();
+			// Set the user's name in the label
 			lblWelcome.setText(lblWelcome.getText().toString() + user.getName());
 		} catch (Exception e) {
-			// TODO: handle exception
 			this.showErrorAlert(e.getMessage());
 		}
 	}
-
+	/**
+	 * Method to handle the log out button action.
+	 * @param event An action event.
+	 */
 	@FXML
 	private void handleLogOutButtonAction(ActionEvent event) {
 		try {
-			// Mostrar panel de confirmación
+			// We show an alert to confirm the log out
 			Alert alert = new Alert(Alert.AlertType.CONFIRMATION, "Are you sure you want to log out?", ButtonType.NO,
 					ButtonType.YES);
 			alert.showAndWait();
+			// If the user clicks yes, we open the login window
 			if (alert.getResult() == ButtonType.YES) {
-				// Abrir login
+				// Load the FXML document
 				FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/LogIn/Login.fxml"));
-
+				// Load the parent
 				Parent root = (Parent) loader.load();
-
+				// Get the controller
 				LoginController controller = (LoginController) loader.getController();
-
+				// Set the stage
 				controller.setStage(stage);
-
+				// Initialize the stage
 				controller.initStage(root);
 			}
 		} catch (Exception e) {
-			// TODO: handle exception
 			this.showErrorAlert(e.getMessage());
 		}
 	}
